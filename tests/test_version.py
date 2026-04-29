e9fdd7dd1ea59159f9c57b61ab388fe2bddb9008
Vladislav Goncharenko <uptrickster@gmail.com>
2026-04-30 00:02:06 +0300

    bumped version waaay more

--- tests/test_version.py
+++ tests/test_version.py
@@ -9,4 +9,4 @@ curr_dir = Path(__file__).resolve().parent
 def test_version():
     pyproject = read_toml(curr_dir / "../pyproject.toml")
 
-    assert somepytools.__version__ == pyproject["tool"]["poetry"]["version"]
+    assert somepytools.__version__ == pyproject["project"]["version"]
